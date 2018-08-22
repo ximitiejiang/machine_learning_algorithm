935a661314a43d07534178afd05c096886aeb9fb
ximitiejiang <ximitiejiang@163.com>
2018-08-22 16:56:14 +0800

    inite common module
    inite common module

--- libCommon/libCommon.py
+++ libCommon/libCommon.py
@@ -3,6 +3,10 @@
 """
 Created on Sun Aug  5 10:00:32 2018
 
+包含的重要子程序包括：
+    - modelsFit(x_train, y_train): 用来循环拟合所有sklearn的模型
+    - learningCurve(X,y,model): 用来拟合和绘制学习曲线
+    
 @author: suliang
 """
 import numpy as np
@@ -48,14 +52,81 @@ def box0(df):
     plt.show()
 
 
-# 测试程序-------------------------------------------    
-a = np.array([1,2,3,4,5,6,7,8,9,10])
-b = np.array([122,126,133,140,145,145,149,150,157,
-              162,166,175,177,177,183,188,199,212])
-c = np.array([102,110,117,118,122,123,132,150])
+def modelsFit(x_train, y_train):    
+    from sklearn.neighbors import KNeighborsClassifier
+    from sklearn.linear_model import LogisticRegression
+    from sklearn.tree import DecisionTreeClassifier
+    from sklearn.ensemble import BaggingClassifier
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.ensemble import AdaBoostClassifier
+    from sklearn.ensemble import GradientBoostingClassifier
+    import xgboost as xgb
+    
+    from sklearn.model_selection import KFold, cross_val_score
+    import datetime
+    
+    starttime = datetime.datetime.now()
+    models = []
+    models.append(('KNN', KNeighborsClassifier(n_neighbors=9)))
+    models.append(('LoR', LogisticRegression()))
+    models.append(('DeT', DecisionTreeClassifier(max_depth=7)))
+    models.append(('Bagging',BaggingClassifier(DecisionTreeClassifier(random_state=1))))
+    models.append(('RanF', RandomForestClassifier(random_state=1)))
+    models.append(('AdaB', AdaBoostClassifier(random_state=1)))
+    models.append(('GBM', GradientBoostingClassifier(learning_rate=0.01,random_state=1)))
+    #models.append(('XGB', xgb.XGBClassifier(random_state=1,learning_rate=0.01)))
 
-d0 = quantile0(b)
-print(f'standard data = {d0}')
+    results = []
+    for name, model in models:
+        kf = KFold(n_splits = 5) # 如果要打乱，则需要固定随机，也就是同时定义shuffle=true, random_state=0
+        cv_score = cross_val_score(model, x_train, y_train, cv=kf)
+        results.append((name, cv_score))
+        print('......{} fit finish'.format(name))
 
-d1 = quantile1(b)
-print(f'calculate data = {d1}')
+    # 显示结果    
+    for i in range(len(results)):
+        print('Model name: {}; CV score: {}'.format(results[i][0], results[i][1].mean()))
+
+    lasttime = datetime.datetime.now() - starttime
+    print('fit finish! last time = {}s'.format(lasttime.seconds))
+    return models, results
+
+
+def learningCurve(X,y,model):
+    from sklearn.model_selection import learning_curve
+    import numpy as np
+    plt.figure(figsize=(6,5), dpi = 80) # figsize定义的是(width, height),千万不要理解成行数列数，否则就编程高，宽了。
+    plt.title('Learning Curve (degrees ={},penalty={})'.format(degrees[1], penalty[0]))
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    plt.ylim([0.8,1.01])
+    from sklearn.model_selection import ShuffleSplit
+
+    # 核心语句1: 交叉验证生成器
+    cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0) # 交叉验证生成器
+    # 核心语句2: 学习曲线参数生成器
+    train_sizes, train_scores, test_scores = \
+            learning_curve(model,   # 带参模型输入
+                           X, y,    # 完整数据集数据输入
+                           cv=cv,   # 交叉验证生成器用ShuffleSplit
+                           train_sizes=np.linspace(.1, 1.0, 5))# 取数据时的size相对位置
+
+    train_scores_mean  = np.mean(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    plt.plot(train_sizes, train_scores_mean, 'o--', color="r",label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",label="Cross-validation score")
+    plt.grid()
+    plt.legend(loc="best")
+
+# ---------------测试程序----------------------------    
+def test():
+    a = np.array([1,2,3,4,5,6,7,8,9,10])
+    b = np.array([122,126,133,140,145,145,149,150,157,
+                  162,166,175,177,177,183,188,199,212])
+    c = np.array([102,110,117,118,122,123,132,150])
+
+    d0 = quantile0(b)
+    print(f'standard data = {d0}')
+
+    d1 = quantile1(b)
+    print(f'calculate data = {d1}')
