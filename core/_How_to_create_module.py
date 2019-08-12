b76ca7f8b93d30c23200a7f4a69f3c38ca9b1bef
ximitiejiang <ximitiejiang@163.com>
2019-08-12 21:35:54 +0800

    update

--- core/_How_to_create_module.py
+++ core/_How_to_create_module.py
@@ -1,29 +1,31 @@
-### 创建pytorch版本的module
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-- 继承自nn.Module
-    ```
-    class VGG(nn.Module)
-        def __init__(self)
-            super().__init__()
-    ```
-- 创建模型容器用nn.Sequential(*list), nn.ModuleList(list), nn.ModuleDict(dict)
+# %% 创建pytorch版本的module
+
+"""继承自nn.Module"""
+import torch.nn as nn
+class VGG(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+"""创建模型容器用nn.Sequential(*list), nn.ModuleList(list), nn.ModuleDict(dict)
     - 如果用nn.Sequential(),则不用自己写内部的forwrd，默认采用串联的层计算结果。
-        ```
-        layers = [nn.Conv2d(3, 64, 3), nn.ReLU()]  #
-        model = nn.Sequential(*layers)        
-        ```
-    - 如果用nn.ModuleList(),则需要自己写forward来自定义内部层的计算过程。
-        ```
-        layers = 
-        model = nn.ModuleList()
-        ```
-    - 如果用nn.ModuleDict(),则需要自己写forward来自定义内部层的计算过程。
-        ```
-        layers = dict(conv1 = nn.Conv2d(), relu1 = nn.ReLU())
-        model = nn.ModuleDict()
-        ```
 
-- 创建每一层，可选择的层包括：
+    - 如果用nn.ModuleList(),则需要自己写forward来自定义内部层的计算过程。
+
+    - 如果用nn.ModuleDict(),则需要自己写forward来自定义内部层的计算过程。
+"""
+layers = [nn.Conv2d(3, 64, 3), nn.ReLU()]  #
+model = nn.Sequential(*layers)       
+
+layers = [nn.Conv2d(3, 64, 3), nn.ReLU()]
+model = nn.ModuleList(layers)
+
+layers = dict(conv1 = nn.Conv2d(), relu1 = nn.ReLU())
+model = nn.ModuleDict(layers)
+
+"""创建每一层，可选择的层包括：
     - nn.Conv2D(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, bias=True)
         上面都是默认值，其中stride为步长，padding为边沿填充，dilation为空洞卷积率，bias为偏置
         其中dilation=n就意味着1x1的核像素变成n*n, 所以dilation=1表示没有空洞。
@@ -48,22 +50,29 @@
     
     - nn.Dropout(p=0.5, inplace=False)
         其中p表示元素变为0的概率，也就是有多少个神经元变为0.
+"""
+layers = nn.Sequential(
+        nn.Conv2d(3, 64, 3, padding=0, stride=1),
+        nn.BatchNorm2d(64),
+        nn.ReLU(inplace=True),
+        nn.MaxPool2d(3, stride=1))
+
         
-- 子模型可以从model.modules(), model.named_modules()来获得，从而进行相关操作，比如初始化
-    ```
-    for m in model.modules():
-        if isinstance(m, nn.Conv2d):
-            xavier_init(m)
-    ```
+"""子模型可以从model.modules(), model.named_modules()来获得，从而进行相关操作，比如初始化"""
 
-- 子模型可以嵌套，通过model.add_module(module_name, nn.Sequential(*layers))
+for m in model.modules():
+    if isinstance(m, nn.Conv2d):
+        xavier_init(m)
 
 
-
-### 创建tensorflow版本的module
+"""子模型可以嵌套，通过model.add_module(module_name, nn.Sequential(*layers))"""
 
 
 
-### 创建caffe版本的module
+#%% 创建tensorflow版本的module
+
+
+
+#%% 创建caffe版本的module
 
 
